81684dec04d4102d96b496493469fd92843ecf5a
abdulrahman-nisar <abdulrahmannisar6@gmail.com>
2026-03-08 00:37:33 +0500

    Add new screens and update resources for image and video selection

--- app/src/main/java/com/example/capai_xml/MainActivity.kt
+++ app/src/main/java/com/example/capai_xml/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.capai_xml
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val selectImageScreenButton = findViewById<Button>(R.id.select_image_screen)
+        val selectVideoScreenButton = findViewById<Button>(R.id.select_video_screen)
+        val imageCaptionPreferencesScreenButton = findViewById<Button>(R.id.image_caption_preferences_screen)
+        selectImageScreenButton.setOnClickListener {
+            val intent = Intent(this, SelectImageScreen::class.java)
+            startActivity(intent)
+        }
+        selectVideoScreenButton.setOnClickListener {
+            val intent = Intent(this, SelectVideoScreen::class.java)
+            startActivity(intent)
+        }
+        imageCaptionPreferencesScreenButton.setOnClickListener {
+            val intent = Intent(this, ImageCaptionPreferencesScreen::class.java)
+            startActivity(intent)
         }
     }
 }
