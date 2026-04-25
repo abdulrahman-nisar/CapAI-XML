f38425b936754329166891855971133cb10aa267
abdulrahman-nisar <abdulrahmannisar6@gmail.com>
2026-04-25 13:41:41 +0500

    Add authentication methods to AuthService and update CapAiDataBase interface

--- app/src/main/java/com/example/capai_xml/data/remote/AuthService.kt
+++ app/src/main/java/com/example/capai_xml/data/remote/AuthService.kt
@@ -1,4 +1,48 @@
 package com.example.capai_xml.data.remote
 
+import com.google.firebase.Firebase
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
+
 class AuthService {
+    val auth = Firebase.auth
+
+    fun signUpWithEmailAndPassword(email: String, password: String, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    onSuccess()
+                } else {
+                    onFailure(task.exception ?: Exception("Unable to sign up"))
+                }
+            }
+    }
+
+    fun signInWithEmailAndPassword(email: String, password: String, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    onSuccess()
+                } else {
+                    onFailure(task.exception ?: Exception("Unable to sign in"))
+                }
+            }
+    }
+
+    fun signInWithGoogle(idToken: String, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    onSuccess()
+                } else {
+                    onFailure(task.exception ?: Exception("Unable to sign in with Google"))
+                }
+            }
+    }
+
+    fun signOut() {
+        auth.signOut()
+    }
+
 }
